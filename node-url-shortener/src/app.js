f3f5d4b5252f11c9badbf927b0f83cd822638420
PedroStabach <pedroaugustostabach3@gmail.com>
2025-08-27 20:29:18 -0300

    banco de dados conectado

---
+++ node-url-shortener/src/app.js
@@ -0,0 +1,7 @@
+const numCharsShortUrl = 7;
+const alfabeto = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+const random = Math.random;
+
+export function generateShortUrl() {
+  
+}
