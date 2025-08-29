90fd50a78595a55e928075ce74a1510b4a27f177
PedroStabach <pedroaugustostabach3@gmail.com>
2025-08-29 01:11:56 -0300

    recomecando

--- node-url-shortener/src/crypto.js
+++
@@ -1,7 +0,0 @@
-const crypto = require('crypto');
-
-function md5Hash(text) {
-  return crypto.createHash('md5').update(text).digest('hex');
-}
-
-console.log(md5Hash("minhaSenha")); // ex: "5f4dcc3b5aa765d61d8327deb882cf99"
