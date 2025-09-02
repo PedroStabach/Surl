7987bc0cc95fa24fbc402728cd106b45b4d4fe68
PedroStabach <pedroaugustostabach3@gmail.com>
2025-09-01 22:20:20 -0300

    del customer sucefful

--- node-url-shortener/src/service/index.js
+++ node-url-shortener/src/service/index.js
@@ -2,6 +2,7 @@ import { group, sleep } from "k6"
 import getCustomer from "../../scenarios/get-customer.js"
 import postCustomer from "../../scenarios/post-customer.js";
 import putCustomer from "../../scenarios/put-customer.js";
+import deleteCustomer from "../../scenarios/delete-customer.js";
 
 export default () => {
     group('endpoint Get Customer - Controller Customer - OnionArchitecture.api', () => {
@@ -13,5 +14,8 @@ export default () => {
     group('endpoint Put Customer - Controler Customer - OnionArchitecture.api', () => {
         putCustomer();
     });
+    group('endpoint Delete Customer - Controler Customer - OnionArchitecture.api', () => {
+        deleteCustomer();
+    })
     sleep(1);
 }
