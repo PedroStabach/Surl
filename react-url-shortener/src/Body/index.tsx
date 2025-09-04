4bd8b2de3086b3bb96ba25de9fea66072f1244b4
PedroStabach <pedroaugustostabach3@gmail.com>
2025-09-03 22:25:00 -0300

    body falso

--- react-url-shortener/src/Body/index.tsx
+++ react-url-shortener/src/Body/index.tsx
@@ -1,14 +1,24 @@
 import styles from './styles.module.css'
 
 export function Body() {
+    //return (
+    //    <>
+    //        <div className={styles.Body}>
+    //            <h1>S.url encurtador de Rotas</h1>
+    //            <h1>Gratis para user</h1>
+    //            <h2>Realiza rotas em menos de 0.2 segundos. Encurte suas rotas com S.url</h2>
+    //            <button>Diminua sua rota</button>
+    //            <button>Urls em massa</button>
+    //        </div>
+    //    </>
+    //)
     return (
         <>
-            <div className={styles.Body}>
-                <h1>S.url encurtador de Rotas</h1>
-                <h1>Gratis para user</h1>
-                <h2>Realiza rotas em menos de 0.2 segundos. Encurte suas rotas com S.url</h2>
-                <button>Diminua sua rota</button>
-                <button>Urls em massa</button>
+            <div className="container">
+            <header>Header</header>
+            <aside>Sidebar</aside>
+            <main>Main Content</main>
+            <footer>Footer</footer>
             </div>
         </>
     )
