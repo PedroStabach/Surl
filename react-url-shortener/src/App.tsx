adab60c4060e24e9672353ce5690efa4ee453b15
PedroStabach <pedroaugustostabach3@gmail.com>
2025-09-04 19:30:18 -0300

    .

--- react-url-shortener/src/App.tsx
+++ react-url-shortener/src/App.tsx
@@ -1,12 +1,10 @@
 import './App.css';
-import './i18n/i18n';  // <-- ESSENCIAL, inicializa o i18next
+import './i18n/i18n'; 
 import { Header } from './header';
 import { Footer } from './footer';
 import { Body } from './Body';
-import { useTranslation } from 'react-i18next';
 
 function App() {
-  const { t, i18n } = useTranslation();
 
   return (
     <>
