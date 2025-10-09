e4306fb839479054b3caf8138b2c50c515dc28b8
PedroStabach <pedroaugustostabach3@gmail.com>
2025-10-09 20:06:49 -0300

    port env.PORT no dist/app.js

--- node-url-shortener/dist/app.js
+++ node-url-shortener/dist/app.js
@@ -6,7 +6,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const routes_1 = __importDefault(require("./Routes/routes"));
 const app = (0, express_1.default)();
-const port = 3000;
+const port = process.env.PORT || 3000;
+
 app.use(express_1.default.json());
 app.use("/api", routes_1.default);
 app.listen(port, () => {
