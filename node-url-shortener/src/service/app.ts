a75722606d8486a3b0b42474d01fdc4f5e720f03
PedroStabach <pedroaugustostabach3@gmail.com>
2025-10-23 22:20:30 -0300

    new frontend

--- node-url-shortener/src/service/app.ts
+++ node-url-shortener/src/service/app.ts
@@ -3,6 +3,7 @@ import cors from 'cors';
 import authRoutes from '../auth/loginRoutes';
 import router from '../Routes/routes';
 import redirectRouter from '../Routes/redirectRouter';
+import path from 'path';
 
 const app = express();
 const port = process.env.PORT || 3000; // Porta dinâmica do Azure
@@ -20,6 +21,11 @@ app.use('/auth', authRoutes);
 app.use(router);
 app.use('/', redirectRouter);
 
+app.use(express.static(path.join(__dirname, '../frontend')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/index.html'));
+});
 app.listen(port, () => {
   console.log(`Servidor rodando na porta ${port}`);
 });
